8a3963c656135e148d50b2bc501776edcfd3e6e6
Patchapon Rochananan <patchapon.r@ku.th>
2021-08-02 16:26:30 +0700

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0c593a4ca07c8538f22c097532017104'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '8c303af9-2d9d-41b1-9531-0922d6c3acaa'
+};
